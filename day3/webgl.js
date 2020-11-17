3df5728d34ef9236f3188463611ea482397f0b80
Agata Kowalska <agekowalska@gmail.com>
2020-11-17 23:44:01 +0100

    [CREATIVE_CODING] Three.js added simple rotating boxes

---
+++ day3/webgl.js
@@ -0,0 +1,80 @@
+// Ensure ThreeJS is in global scope for the 'examples/'
+global.THREE = require('three');
+
+// Include any additional ThreeJS examples below
+require('three/examples/js/controls/OrbitControls');
+
+const canvasSketch = require('canvas-sketch');
+const pallets = require('nice-color-palettes');
+const random = require('canvas-sketch-util/random');
+
+const settings = {
+  // Make the loop animated
+  dimensions: [512, 512],
+  fps: 24,
+  duration: 4,
+  animate: true,
+  // Get a WebGL canvas rather than 2D
+  context: 'webgl',
+};
+
+const sketch = ({ context, innerHeight, innerWidth }) => {
+  const pallet = random.pick(pallets);
+
+  const renderer = new THREE.WebGLRenderer({
+    context,
+  });
+
+  renderer.setClearColor('#fff', 1);
+  renderer.setSize(innerHeight, innerWidth);
+
+  const scene = new THREE.Scene();
+
+  const firstCube = new THREE.Mesh(
+    new THREE.BoxGeometry(5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: random.pick(pallet) })
+  );
+
+  scene.add(firstCube);
+
+  const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: random.pick(pallet) })
+  );
+
+  scene.add(cube);
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  const light = new THREE.PointLight('#45caf7', 1, 15.5);
+
+  scene.add(light);
+
+  camera.position.z = 15;
+
+  return {
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
+      renderer.setPixelRatio(pixelRatio);
+      renderer.setSize(viewportWidth, viewportHeight);
+    },
+    // And render events here
+    render() {
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      firstCube.rotation.x -= 0.01;
+      firstCube.rotation.y -= 0.01;
+      renderer.render(scene, camera);
+    },
+    // Dispose of WebGL context (optional)
+    unload() {
+      renderer.dispose();
+    },
+  };
+};
+
+canvasSketch(sketch, settings);
