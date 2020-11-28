dc2eaefb1c9a3a865aa35e793afd9d9c0fccfa5b
Agata Kowalska <agekowalska@gmail.com>
2020-11-28 21:15:23 +0100

    [CREATIVE CODING] added day9

--- day9/sketch.js
+++ day9/sketch.js
@@ -1,5 +1,4 @@
 const canvasSketch = require('canvas-sketch');
-const random = require('canvas-sketch-util/random');
 const pallets = require('nice-color-palettes');
 
 // Grab P5.js from npm
@@ -8,44 +7,86 @@ const p5 = require('p5');
 // Attach p5.js it to global scope
 new p5();
 
+
+const colors = [
+  '#EF476F',
+  '#FFD166',
+  '#06D6A0',
+  '#118AB2',
+  '#073B4C'
+]
+
 const settings = {
   p5: true,
-  dimensions: [512, 512],
+  dimensions: [1080, 1080],
 };
 
 let margin = 25;
-let marginX = 25;
-let marginY = 25;
 let w, h;
 let c, r;
-let arcX;
-let arcY;
 let cellWidth, cellHeight;
+let foreground;
 
-const renderCell = (row, column) => {
-  rectMode(CORNER);
 
-  let x = margin+cellWidth*row;
-  let y = margin+cellHeight*column;
-
-  rect(x, y, cellWidth, cellHeight)
+const generateBackground = () => {
+  for (let i = 0; i < width; i += 20) {
+    for (let j = 0; j < height; j += 20) {
+      stroke('#fff');
+      fill('#118AB2');
+      rect(i, j, 20, 20);
+    }
+  }
 }
+
+const generateRandomStyle = (start, end) => {
+
+  const fillRandomVal = floor(random(0, 5));
+  const generatedColor = colors[fillRandomVal]
+
+  foreground.stroke('#073B4C')
+  foreground.strokeWeight(4);
+  foreground.fill(generatedColor);
+
+}
+
+const generateForeground = () => {
+  for (let i = 0; i < r; i++) {
+    for (let j = 0; j < c; j++) {
+      renderCell(i, j);
+    }
+  }
+}
+
 
 const renderRandomShape = (shapeX, shapeY) => {
-  rectMode(CENTER);
+  let start = random(0,  360);
+  let stop = random(0, 360);
 
-  let newArcX = (marginX + shapeX) / 2;
-  let newArcY = (marginY + shapeY) / 2;
+  generateRandomStyle(start, stop);
+  if (abs(stop - start < 90)) {
+    stop = random(0, 360);
+  }
 
-  rect(newArcX, newArcY, cellWidth/2, cellHeight/2);
+  let ran = random(0, 10);
+  let shape = (ran < 5) ? CHORD : PIE;
+  
+  foreground.blendMode(REMOVE);
+  foreground.arc(shapeX, shapeY, cellWidth - 25, cellHeight - 25, start, stop, shape);
 
-  marginX += (cellWidth / 4)
-  marginY += (cellHeight / 4);
+  foreground.blendMode(BLEND);
 
+  if ((ran < 5)) {
+    foreground.noFill();
+  }
+  foreground.arc(shapeX, shapeY, cellWidth - 25, cellHeight - 25, start, stop, shape);
+
+ 
 }
 
-const generateRandomStyle = () => {
-
+const renderCell = (row, column) => {
+  let x = margin+cellWidth*row + cellWidth/2;
+  let y = margin+cellHeight*column + cellWidth/2
+  renderRandomShape(x, y)
 }
 
 const sketch = () => {
@@ -55,15 +96,22 @@ const sketch = () => {
   r = 4;
   cellWidth = w/c;
   cellHeight = h/r;
+
+  foreground = createGraphics(width, height);
+
+
   return ({ context, width, height }) => {
+    foreground.background('#073B4C');
+    foreground.strokeWeight(3);
 
 
-    for (let i = 0; i < r; i++) {
+    generateForeground();
+  
 
-      for (let j = 0; j < c; j++) {
-        renderRandomShape(cellWidth * (i+1), cellHeight *(j+1));
-      }
-    }
+    generateBackground();
+
+    image(foreground, 0, 0);
+
 
   };
 };
